6a1fd8a39792c3d619367e112a4c8a82642c413f
Bajtix <bajtix.b@gmail.com>
2021-10-19 14:37:06 +0200

    calendar rework step 1

--- LibrusCalendar.cs
+++ LibrusCalendar.cs
@@ -6,65 +6,51 @@ using HtmlAgilityPack;
 
 namespace BrusLib {
     public class LibrusCalendar {
-        public readonly Dictionary<DateTime, List<SchoolEvent>> events;
+        private const string requestUrl = "https://synergia.librus.pl/terminarz";
         
-        public LibrusCalendar(Dictionary<DateTime, List<SchoolEvent>> events) {
-            this.events = events;
-        }
+        public static async Task<LibrusCalendar> Retrieve(LibrusConnection connection, APIBufferMode bufferMode = APIBufferMode.none) {
+            string html = "";
 
-        public static async Task<LibrusCalendar> Retrieve(LibrusConnection connection) {
-            string html = await Util.FetchAsync("https://synergia.librus.pl/terminarz", connection.cookieSession,
-                Util.SYNERGIA_INDEX);
-            
-            //File.WriteAllText("terminarz req.html",html);
-            /* File.ReadAllText("terminarz req.html");*/
+            switch (bufferMode) {
+                case APIBufferMode.none:
+                    html = await Util.FetchAsync(requestUrl, connection.cookieSession,
+                        Util.SYNERGIA_INDEX);
+                    break;
+                case APIBufferMode.load:
+                    if (File.Exists("buffer_calendar")) {
+                        html = File.ReadAllText("buffer_calendar");
+                        break;
+                    }
+                    else
+                        goto case APIBufferMode.save;
+                case APIBufferMode.save:
+                    html = await Util.FetchAsync(requestUrl, connection.cookieSession,
+                        Util.SYNERGIA_INDEX);
+                    File.WriteAllText("buffer_calendar", html);
+                    break;
+            }
             
             var doc = new HtmlDocument();
             doc.LoadHtml(html);
             var document = doc.DocumentNode;
 
-            var calRoot = document.SelectSingleNode("/html/body/div[3]/div[3]/form/div/div/div/table");
-            var days = calRoot.SelectNodes("//div[@class=\"kalendarz-dzien\"]");
+            var tableNode = document.SelectSingleNode("/html/body/div[3]/div[3]/form/div/div/div/table");
+            var days = tableNode.SelectNodes(".//div[@class=\"kalendarz-dzien\"]");
 
-            var results = new Dictionary<DateTime, List<SchoolEvent>>();
-            
-            foreach (var day in days) {
-                var ls = new List<SchoolEvent>();
-                int d = int.Parse(day.SelectSingleNode("./div").InnerText);
-                var date = new DateTime(DateTime.Now.Year, DateTime.Now.Month, d); // huge mess; TODO: cleanup?
-                
-                var events = day.SelectNodes(".//tr");
+            foreach (var d in days) {
+                var date = d.SelectSingleNode("./div[@class=\"kalendarz-numer-dnia\"]");
+                var events = d.SelectNodes(".//tr");
+
+                Console.WriteLine(date.InnerText + "-----------------");
                 if(events == null) continue;
                 foreach (var e in events) {
-                    string it = e.SelectSingleNode("./td").InnerHtml;
-                    string tt = e.SelectSingleNode("./td").GetAttributeValue("title", "");
-                    ls.Add(IdentifyEvent(it,tt,date ));
+                    Console.WriteLine(e.InnerText.Trim());
                 }
-                results.Add(date, ls);
+                
+                
             }
             
-            return new LibrusCalendar(results);
+            return new LibrusCalendar();
         }
-
-        private static SchoolEvent IdentifyEvent(string innerText, string descriptionText, DateTime w) {
-            if (innerText.Contains("Praca klasowa") || innerText.Contains("Kartkówka") || innerText.Contains("Inne")) {
-                return new SEExam(innerText + "<br>" + descriptionText, w);
-            }
-            else
-                return new SchoolEvent(innerText + "<br>" + descriptionText, w);
-        }
-
-        public List<SchoolEvent> GetAllEvents() {
-            List<SchoolEvent> es = new List<SchoolEvent>();
-            foreach (var i in events) {
-                es.AddRange(i.Value);
-            }
-
-            return es;
-        }
-        
-        
-        
-        
     }
 }
